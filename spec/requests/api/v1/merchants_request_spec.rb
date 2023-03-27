ff63ccbf4e70f8881310c3f82e940b76868d6f2b
KaraJH <karas.jones@yahoo.com>
2023-03-27 15:23:35 -0600

    test: Merchant show request

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -27,4 +27,34 @@ RSpec.describe "Merchant API" do
       end
     end
   end
+
+  describe "get one merchant" do 
+    it "happy path - can get one merchant with an id" do 
+      id = create(:merchant).id 
+
+      get "/api/v1/merchants/#{id}"
+
+      merchant = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response).to be_successful
+
+      expect(merchant).to have_key(:data)
+      expect(merchant[:data]).to have_key(:id)
+      expect(merchant[:data]).to have_key(:type)
+      expect(merchant[:data]).to have_key(:attributes)
+      expect(merchant[:data][:attributes]).to have_key(:name)
+      expect(merchant[:data][:id]).to be_a(String)
+      expect(merchant[:data][:type]).to eq("merchant")
+      expect(merchant[:data][:attributes]).to be_a(Hash)
+      expect(merchant[:data][:attributes][:name]).to be_a(String)
+    end
+
+    it "sad path - is not succuessful with an incorrect id" do 
+      # merchant = merchant_with_items
+      # item_id = merchant.items.first.id
+      create_list(:merchant, 3)
+    
+      expect {get "/api/v1/merchants/9999"}.to raise_error(ActiveRecord::RecordNotFound)
+    end
+  end
 end
