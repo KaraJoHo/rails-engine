2204fc2fe37962ec8cb315d8973d8bb81bd3c0cd
KaraJH <karas.jones@yahoo.com>
2023-03-28 12:57:30 -0600

    feat: Find by name query

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ApplicationRecord 
   belongs_to :merchant 
 
-  validates_presence_of :name, :description 
+  validates_presence_of :name, :description, :unit_price
   validates_numericality_of :unit_price
 end
