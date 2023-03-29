b2b10262b2209a598dc388a0d2185de496d9fd13
KaraJH <karas.jones@yahoo.com>
2023-03-29 16:51:57 -0600

    feat: Add before destroy for item to delete invoice items

---
+++ app/models/invoice.rb
@@ -0,0 +1,12 @@
+class Invoice < ApplicationRecord 
+  belongs_to :merchant
+  belongs_to :customer
+  has_many :invoice_items, dependent: :destroy
+  has_many :items, through: :invoice_items 
+
+  validates_presence_of :status
+
+  def self.only_one_item 
+    Invoice.joins(:items).group('invoices.id').having('count(invoice_items.invoice_id) = 1')
+  end
+end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,9 +1,18 @@
 class Item < ApplicationRecord 
   belongs_to :merchant 
+  before_destroy :delete_invoices_from_item 
+
+  has_many :invoice_items, dependent: :destroy
+  has_many :invoices, through: :invoice_items
 
   validates_presence_of :name, :description, :unit_price
   validates_numericality_of :unit_price
 
+  def delete_invoices_from_item 
+    invoices.only_one_item.each do |invoice| 
+      invoice.destroy!
+    end
+  end
 
   def self.find_by_name_search(name_fragment)
     where("lower(name) ILIKE?", "%#{name_fragment}%").order(:name)
