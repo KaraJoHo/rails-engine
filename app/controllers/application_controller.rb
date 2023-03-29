cda918ec722976f532c4be5e01da1290dbeb4cc6
KaraJH <karas.jones@yahoo.com>
2023-03-29 13:05:26 -0600

    feat: Add resur from for errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,12 @@
 class ApplicationController < ActionController::API
-
   rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   def render_unprocessable_entity_response(exception)
-    render json: exception.record.errors, status: :unprocessable_entity, status: 404
+    render json: ErrorSerializer.bad_params(exception.record.errors), status: 404
+  end
+
+  def render_not_found_response(exception)
+    render json: {error: exception.message, message: "your query could not be completed" }, status: :not_found 
   end
 end
