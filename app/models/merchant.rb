8ca46d4e31f13f1de850672c178d9ab01c81635c
KaraJH <karas.jones@yahoo.com>
2023-03-28 18:14:49 -0600

    feat: Search name query method

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -4,6 +4,6 @@ class Merchant < ApplicationRecord
   validates_presence_of :name
 
   def self.find_by_name_query(name_fragment)
-    where("lower(name) ILIKE?", "%#{name_fragment}%").first
+    where("lower(name) ILIKE?", "%#{name_fragment}%").order(:name).first
   end
 end
