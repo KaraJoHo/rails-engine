f5acfc8dd3d8039d7d02f8c619e72b43169b0834
KaraJH <karas.jones@yahoo.com>
2023-03-28 18:14:30 -0600

    feat: Search criteria model methods(name, min, max, price between)

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,21 @@ class Item < ApplicationRecord
 
   validates_presence_of :name, :description, :unit_price
   validates_numericality_of :unit_price
+
+
+  def self.find_by_name_search(name_fragment)
+    where("lower(name) ILIKE?", "%#{name_fragment}%").order(:name)
+  end
+
+  def self.find_by_min_price(number)
+    where('unit_price >= ?', number.to_f).order(:name)
+  end
+
+  def self.find_by_max_price(number)
+    where('unit_price <= ?', number.to_f).order(:name)
+  end
+
+  def self.find_by_price_between(max, min)
+    where('unit_price <= ?', max.to_f).where('unit_price >= ?', min.to_f).order(:name)
+  end
 end
